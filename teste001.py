b83750d12f4280e13e4b16f99b1ba866ae905f5a
HiginoDev <higino.coelho@gmail.com>
2022-04-08 22:45:52 -0300

    Primeiro commit

---
+++ teste001.py
@@ -0,0 +1,20 @@
+#d = float(input('Quanto dinheiro você dispõe para comprar dólares?'))
+#dolar = 3.27
+#r = d/dolar
+#print('Com a cotação de hoje você pode comprar: {:.2f} dolares'.format(r))
+
+
+#a = float(input('Digite a altura da parede a ser pintada:'))
+#l = float(input('Digite a largura da parede a ser pintada:'))
+#area = a*l
+#t = area/2
+#print('Sua parede tem {:.2f} metros quadradados e você vai precisar de {:.2f} litros de tinta para completar a pintura'.format(area,t))
+
+#p = float(input('Digite o valor do produto:'))
+#d = p/100*5
+#novo = p-d
+#print('O valor do produto com desconto de 5% é R${}'.format(novo))
+
+s = float(input('Digite o valor do seu salário atual:'))
+p = s*115/100
+print('Ao receber 15% de aumento seu salário será de R${:.2f}'.format(p))
